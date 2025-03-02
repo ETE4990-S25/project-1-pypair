468d7076b44251fb44164c73af99ff1b54ffce7a
Namho Kye <namkye77@gmail.com>
2025-03-01 21:24:17 -0800

    Menu template for user selection

--- Project_Main_Function/Menu.py
+++ Project_Main_Function/Menu.py
@@ -7,6 +7,20 @@ def display_menu(options):
     for item in menu_generator(options):
         print(item)
 
+def handle_choice(options):
+    while True:
+        try:
+            choice = int(input("Select an Option:"))
+            if 1 <= choice <= len(options):
+                print(f"You Chose {options[choice - 1]}")
+                break
+            else:
+                print(f"Invalid Choice")
+        except ValueError:
+            print("Invalid Input")
+    
+
 if __name__ == "__main__":
     menu_option = ["Start Game", "Load Game", "Exit Game"]
     display_menu(menu_option)
+    handle_choice(menu_option)
